3167f1f0fec97470cb7f93f13b2c7757a0a02d79
Jorge Ruiz <jorgeruiz@mail.com>
2018-02-14 16:08:31 +0100

    segundo ejercicio

---
+++ ejer2.py
@@ -0,0 +1,3 @@
+﻿anyo = input("Escribe el año que naciste: ")
+edad = 2018 - anyo
+print "Tienes " + str(edad) + " años"
---
+++ ejer2a.py
@@ -0,0 +1,6 @@
+x = input("Dime un numero: ")
+y = input("Dime otro numero: ")
+while  x<=y:
+	if x%2==0:
+		print x
+	x += 1
