9e1a97a1e2371dde63ce471923181157ba265744
Jorge Ruiz <jorgeruiz@mail.com>
2018-02-14 16:22:42 +0100

    actualizacion ejercicio

--- ejer2a.py
+++ ejer2a.py
@@ -1,7 +1,5 @@
-x = raw_input("Dime un numero: ")
-y = raw_input("Dime otro numero: ")
-x = int(x)
-y = int(y)
+x = int(raw_input("Dime un numero: "))
+y = int(raw_input("Dime otro numero: "))
 while  x<=y:
 	if x%2==0:
 		print x
